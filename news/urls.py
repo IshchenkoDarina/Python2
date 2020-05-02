f6f5198053e248614dd61d87f717f4632c5d33ba
Darina Ishchenko <darina.ishchenko17@gmail.com>
2020-05-02 21:56:03 +0300

    added news post page

--- news/urls.py
+++ news/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$',
         ListView.as_view(queryset=Articles.objects.all().order_by('-date')[:20],
                          template_name='news/posts.html')),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Articles, template_name='news/post.html')),
 ]
